49e80456ee93afd2ee1bda8cb12fee4c930a86a9
TBecke <tbecke@progeek.de>
2024-04-24 12:02:43 +0200

    Research

--- src/components/Research/ResearchList.tsx
+++ src/components/Research/ResearchList.tsx
@@ -1,8 +1,21 @@
 import Box from "@mui/material/Box";
 import {researchList} from "../../model/ResearchList.ts";
 import ResearchBox from "./ResearchBox.tsx";
+import {useGame} from "../../context/GameContext.ts";
+import {Research} from "../../model/Research.ts";
+import {useEffect, useState} from "react";
 
 const Research = () => {
+    const {game} = useGame()
+    const [filteredResearchList, setFilteredResearchList] = useState<Research[]>([]);
+
+    useEffect(() => {
+        const filteredList = researchList.slice(0, 3).filter(research => {
+            return game.researches[research.id] === 0;
+        });
+        setFilteredResearchList(filteredList);
+    }, [game.researches]);
+
     return (
         <Box sx={{
             width: {lg: '100%'},
@@ -15,7 +28,7 @@ const Research = () => {
             alignItems: 'center',
             gap: 2
         }}>
-            {researchList.map((research) => (
+            {filteredResearchList.map((research) => (
                 <ResearchBox key={research.id} research={research}/>
             ))}
         </Box>

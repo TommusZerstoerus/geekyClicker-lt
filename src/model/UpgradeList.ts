846efff76a1fa8bc9d33630a677efaa32d431d50
TBecke <tbecke@progeek.de>
2024-04-30 09:14:24 +0200

    balancing

--- src/model/UpgradeList.ts
+++ src/model/UpgradeList.ts
@@ -12,16 +12,16 @@ export const UpgradeBonusList: Record<number, number> = {
 }
 
 export const UpgradeMileStoneList: Record<number, number> = {
-    0: 100, // Kotlin lernen
-    1: 250, // Cache leeren
-    2: 500, // Merge Konflikte lösen
-    3: 1000, // Boris nach Hilfe fragen
-    4: 5000, // Force Push auf Main
-    5: 500, // Einen weiteren Monitor aufstellen
-    6: 1000, // Musik hören
-    7: 10000, // Git Pipeline schreiben
-    8: 50000, // Kaffeevollautomat kaufen
-    9: 100000 // Python verbieten
+    0: 1000, // Kotlin lernen
+    1: 2500, // Cache leeren
+    2: 5000, // Merge Konflikte lösen
+    3: 10000, // Boris nach Hilfe fragen
+    4: 50000, // Force Push auf Main
+    5: 5000, // Einen weiteren Monitor aufstellen
+    6: 10000, // Musik hören
+    7: 100000, // Git Pipeline schreiben
+    8: 500000, // Kaffeevollautomat kaufen
+    9: 1000000 // Python verbieten
 }
 
 export const BasePriceList: Record<number, number> = {
